eb3bff23595cce4256b285a32644fb348d742653
исп-31 <-31@LAB24-08>
2021-11-12 10:11:24 +0300

    Добавьте файлы проекта.

---
+++ Lib_8/ProgramModules.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Lib_8
+{
+    public class ProgramModules
+    {
+        //Random random = new Random();
+        public static void SumEvenWithOut(int count, out int max, out string numbers)
+        {
+            Random random = new Random();
+            max = 0;
+            numbers = string.Empty;
+
+            for (int i = 0; i < count; i++)
+            {
+                int number = random.Next(0, count);
+                numbers += number.ToString() + " ";
+                if (max <= number)
+                {
+                    max = number;
+                }
+
+
+
+            }
+        }
+
+
+        public (int sum, string numbers) SumEvenWithTuple(int count)
+        {
+            Random random = new Random();
+            int max = 0;
+            string numbers = string.Empty;
+
+            for (int i = 0; i < count; i++)
+            {
+                int number = random.Next(0, count);
+                numbers += number.ToString() + " ";
+                if (max <= number)
+                {
+                    max = number;
+                }
+            }
+
+            return (max, numbers);
+        }
+    }
+}
---
+++ практическа работа номер 1/MainWindow.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Lib_8;
+
+namespace практическа_работа_номер_1
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+
+        private void ResultOutput(object sender, RoutedEventArgs e)
+        {
+            arrayOutput.Clear();
+            bool f = Int32.TryParse(inputBox.Text, out int tryParseResult);
+            if (f == true)
+            {
+                ProgramModules.SumEvenWithOut(Convert.ToInt32(inputBox.Text), out int max, out string numbers);
+                for (int i = 0; i < numbers.Length; i++)
+                {
+                    arrayOutput.Text += numbers[i].ToString() + " ";
+
+                }
+                maximalNumber.Text = max.ToString();
+            }
+            else
+            {
+                MessageBox.Show("Ошибка");
+            }
+
+        }
+
+        private void Exit_Button(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void Info_Button(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Выполнил работу Селиванов Егор ИСП-31 Найти максимум из n целых случайных чисел X, распределенных в диапазоне от 0 до n.Вывести на экран на одной строке сгенерированные числа, на другой строкерезультат.");
+        }
+    }
+}
